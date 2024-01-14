a64eed36743ae54f1993c20e166a0113bcffda6c
Daniel Segall <segalld@gmail.com>
2024-01-14 17:55:23 +0000

    Cleanup

--- Sources/swift-fsm-macros-event/swift_fsm_macros_eventMacro.swift
+++ Sources/swift-fsm-macros-event/swift_fsm_macros_eventMacro.swift
@@ -3,16 +3,28 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
+extension ExprSyntax {
+    func formatted(functionName: String) -> ExprSyntax {
+        "static let \(self) = \(raw: functionName)(\"\(self)\")"
+    }
+}
+
+extension FreestandingMacroExpansionSyntax {
+    var firstArgument: ExprSyntax {
+        guard let argument = argumentList.first?.expression else {
+            fatalError("compiler bug: the macro does not have any arguments")
+        }
+
+        return argument
+    }
+}
+
 public struct EventMacro: ExpressionMacro {
     public static func expansion(
         of node: some FreestandingMacroExpansionSyntax,
         in context: some MacroExpansionContext
     ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
-
-        return "static let \(argument) = event(\"\(argument)\")"
+        node.firstArgument.formatted(functionName: "event")
     }
 }
 
@@ -21,11 +33,7 @@ public struct EventWithValueMacro: ExpressionMacro {
         of node: some FreestandingMacroExpansionSyntax,
         in context: some MacroExpansionContext
     ) -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
-            fatalError("compiler bug: the macro does not have any arguments")
-        }
-
-        return "static let \(argument) = eventWithValue(\"\(argument)\")"
+        node.firstArgument.formatted(functionName: "eventWithValue")
     }
 }
 

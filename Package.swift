27346de2dcca5abef4669326ea20bf2a542a5c08
Daniel Segall <segalld@gmail.com>
2024-01-14 17:13:30 +0000

    Remove junk from project

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
             name: "swift-fsm-macros",
             targets: ["swift-fsm-macros"]
         ),
-        .executable(
-            name: "swift-fsm-macrosClient",
-            targets: ["swift-fsm-macrosClient"]
-        ),
     ],
     dependencies: [
         // Depend on the Swift 5.9 release of SwiftSyntax
@@ -37,8 +33,6 @@ let package = Package(
         // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "swift-fsm-macros", dependencies: ["swift-fsm-macrosMacros"]),
 
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "swift-fsm-macrosClient", dependencies: ["swift-fsm-macros"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
--- Tests/swift-fsm-macrosTests/swift_fsm_macrosTests.swift
+++ Tests/swift-fsm-macrosTests/swift_fsm_macrosTests.swift
@@ -1,19 +1,14 @@
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 import XCTest
-
-// Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
-#if canImport(swift_fsm_macrosMacros)
 import swift_fsm_macrosMacros
 
 let testMacros: [String: Macro.Type] = [
     "stringify": StringifyMacro.self,
 ]
-#endif
 
 final class swift_fsm_macrosTests: XCTestCase {
     func testMacro() throws {
-        #if canImport(swift_fsm_macrosMacros)
         assertMacroExpansion(
             """
             #stringify(a + b)
@@ -23,13 +18,9 @@ final class swift_fsm_macrosTests: XCTestCase {
             """,
             macros: testMacros
         )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
     }
 
     func testMacroWithStringLiteral() throws {
-        #if canImport(swift_fsm_macrosMacros)
         assertMacroExpansion(
             #"""
             #stringify("Hello, \(name)")
@@ -39,8 +30,5 @@ final class swift_fsm_macrosTests: XCTestCase {
             """#,
             macros: testMacros
         )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
     }
 }
